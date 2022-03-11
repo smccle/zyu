df013159a326bf03bd78ec26f020dcebafc92409
ZYTK Devs <97977626+z1tk@users.noreply.github.com>
2022-03-11 14:05:49 -0500

    Update watch.js

--- watch/js/watch.js
+++ watch/js/watch.js
@@ -65,7 +65,7 @@ function loadJSON(path, success, error) {
 	document.title = Data.title + " - " + Data.author_name;
 	setCookie("lwvt", Data.title, 60);
 	setCookie("lwv", id, 60);
-  setCookie("history", getCookie("history") + Data.title + ":" + id + ":");
+  setCookie("history", getCookie("history") + Data.title + ":" + id + ":", 60);
   }
 
 var queryString = new Array();
